10151dbf3660b3dfdfceb9b02abb279ab9ebce3a
natenhagen <natepappenhagen@gmail.com>
2018-06-14 16:38:22 -0600

    Verbal questions

---
+++ solutions.js
@@ -0,0 +1,213 @@
+console.log('hello');
+
+// ### Verbal questions
+
+// What is the difference between a **parameter** and an **argument**?
+
+// parameter = is what is in parenthesis when you DECLARE a function
+// arguement = is what is in parenthesis when you CALL a function
+// difference? = parameters are like the recipe and arguments are the instance
+
+
+// Within a function, what is the difference between **return** and **console.log**?
+
+// return =
+// console.log =
+// difference?
+
+
+
+
+// "Commit 1 - Verbal questions"
+
+
+
+
+// ## Palindrome
+// Write a function `checkPalindrome` that accepts a single argument, a string.
+//
+//  The function should return true (Boolean) if the string is a palindrome,
+//
+//   false if it is not.
+//
+//    Make sure your function will give the correct answer for words with **capital letters**.
+
+
+
+
+
+
+
+
+
+//                      console.log(checkPalindrome("Radar")); => true
+//                      console.log(checkPalindrome("Borscht")); => false
+
+
+// "Commit 2 - Palindrome".
+
+
+
+
+
+
+
+
+// ## Digit Sum
+// Write a function `sumDigits` that accepts a number
+//                    and returns the sum of its digits.
+
+// console.log(sumDigits(42));    => 6;
+
+
+
+
+
+
+
+
+
+
+
+
+// "Commit 3 - Digit Sum".
+
+
+
+
+
+// ## Pythagoras
+// Write a function `calculateSide` that takes two arguments: `sideA` and `sideB`,
+//               and returns the solution for sideC using the Pythagorean theorem.
+
+// _hint:_ discover the Pythagorean Theorem on a website called google.com
+// _hint:_ checkout the [Math methods](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math) in javascript
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(calculateSide(8, 6));    => 10
+
+
+// "Commit 4 - Pythagoras".
+
+
+
+// ## Sum Array
+// Write a function `sumArray` that takes an (array) as an argument.
+// The array should contain numbers. The function should return the sum of the numbers in the array.
+// Use a _for loop_ within the function to iterate over the array and sum the contents.
+// Use a variable such as
+//
+//
+// let sum = 0;
+
+// that will **accumulate** value within the loop.
+//Expected result: console.log(sumArray([1, 2, 3, 4, 5, 6]));  => 21
+
+
+
+
+
+
+
+
+
+
+
+// "Commit 5 - Sum Array".
+
+
+
+
+
+
+// ## Prime Numbers
+// A Prime number is a number that is not evenly divisible by another number
+// except 1 and itself. If you want to read more deeply about it,
+//  [go here](https://en.wikipedia.org/wiki/Prime_number).
+// To test whether a number is Prime, you only need to test as
+// far as the **square root** of that number.
+//  This is advisable for optimization and testing large numbers.
+
+
+// ### Step One
+// Write a function called `checkPrime` that will test whether a number is Prime.
+//  The function will return true (Boolean) if Prime, false if not.
+// _Hint:_ Check every number up to the square root. To do this, try a _for loop_.
+
+
+
+
+
+
+
+
+// ### Step Two
+// Write another function called `printPrimes` that will print (console log)
+//  all the Primes up to an arbitrary limit. For example, if you invoke your
+//   function with `printPrimes(97)`, it will print all the Prime numbers up
+//    to and including 97.
+//
+// This function can **call on** the previous `checkPrime` function.
+
+
+
+
+
+
+
+// "Commit 6 - Prime Numbers".
+
+
+
+
+
+
+
+
+// ## Insert Dash
+// Write a function `insertDash` that accepts a number as a parameter
+//  and returns a string with a dash inserted between any consecutive
+//   **odd numbers**. There should not be a dash at the end,
+//    it goes only between numbers.
+
+
+//  console.log(insertDash(454793));   => 4547-9-3
+
+
+
+
+
+
+
+// "Commit 7 - Insert Dash".
+
+
+
+
+// # CSS
+// Watch the following three videos on CSS:
+//
+// - [First CSS video - 5 minutes](https://www.youtube.com/watch?v=xWiT2TWCFjc&index=3&list=PLdnONIhPScST0Vy4LrIZiYKpFNoxgyH7J)
+// - [Second CSS video - 11 minutes](https://www.youtube.com/watch?v=UMMHsQPmfug&index=4&list=PLdnONIhPScST0Vy4LrIZiYKpFNoxgyH7J)
+// - [Third CSS video - 17 minutes](https://www.youtube.com/watch?v=g0Aq2kP5-CY&index=5&list=PLdnONIhPScST0Vy4LrIZiYKpFNoxgyH7J)
+//
+// You will need to have watched these videos for tomorrow's lab.
+
+// # Hungry for more
+//
+// 1. Complete the afternoon lab.
+// 2. [Project euler](https://projecteuler.net/archives)
+
+
+
+// "Commit 8 - Hungry for More".
