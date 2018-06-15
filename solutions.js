124461ff96957051980fd7883e03727ad0b79c7a
natenhagen <natepappenhagen@gmail.com>
2018-06-15 15:01:57 -0600

    Commit 5 - Sum Array

--- solutions.js
+++ solutions.js
@@ -124,13 +124,15 @@ console.log(calculateSide(8, 6));
 //Expected result: console.log(sumArray([1, 2, 3, 4, 5, 6]));  => 21
 
 
+const sumArray = (arr) => {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		    sum = arr[i] + sum;
+	}
+   return sum
+}
 
-
-
-
-
-
-
+console.log(sumArray([2, 4, 6, 8, 10,]));
 
 
 // "Commit 5 - Sum Array".
